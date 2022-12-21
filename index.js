4c4e59b8110b244c1795c13455a33137edc7d124
merlijn1411 <34530@ma-web.nl>
2022-12-21 15:34:22 +0100

    les 8 en 9 toevoeging

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-
-
-
-
-
-
-class MijnClass
+class Player
 {
-    constructor()
+    constructor(index)
     {
         this.dive = div;
         this.atTile = 0;
@@ -14,13 +8,9 @@ class MijnClass
         className.style.display = "block";
 
     }
-    mijnFunctie(a, b)
-    {
-        return a + b;
-    }
 }
 
-class tile
+class Tile
 {
     constructor(div)
     {
@@ -38,11 +28,104 @@ class Game
         this.playerturnDiv = document.getElementsByClassName("playerturn")[0];
         this.rollDiv = document.getElementsByClassName("roll")[0];
         this.mainDiv = document.getElementsByClassName("main")[0];
-        this.boardDiv = document.getElementsByClassName("board")[0];
+        this.boardDiv = document.getElementById("board");
         this.boardoverlayDiv = document.getElementsByClassName("selectplayer")[0];
-
         this.tile =[];
         this.players = [];
         this.playerturn = 0;
+        this.setupBoard();
     }
+
+    setupBoard()
+    {
+        //1 = right, 0 = up, 3 = left 
+        let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+                0, 3, 3, 3, 3, 3, 3, 3, 3, 0,
+                0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                0, 3, 3, 3, 3, 3, 3, 3, 3, 0,
+                0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+        let x = 0;
+        let y = 10;
+        let tileSize = 55;
+
+        for	(var i = 0; i < path.length; i++)
+        {
+            console.log("test2");
+            let cmd = path[i];
+            if (cmd == 1)
+            {
+                //right
+                x++;
+            }
+            else if (cmd == 3)
+            {
+                //left
+                x--;
+            }
+            else if (cmd == 0)
+            {
+                //up
+                y--;
+            }
+
+            let div = this.makeBoardDiv(x * tileSize, y * tileSize, i+1)
+
+            let tile = new Tile(div);
+            this.tile.push(tile);
+
+        }
+    }
+
+
+
+    SetupGotos()
+    {
+
+    }
+
+    Start(amountOffPlayers)
+    {
+
+    }
+
+    moveToNextPlayer()
+    {
+
+    }
+
+    draw()
+    {
+
+    }
+
+    roll()
+    {
+
+    }
+
+    setpawn(playerI, atTile)
+    {
+
+    }
+
+    makeBoardDiv(x, y, tileDisplayNumber)
+    {
+        let div = document.createElement("div");
+
+        div.className="tile";
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        
+        div.textContent = tileDisplayNumber;
+        this.boardDiv.appendChild(div);
+
+        return div;
+    }
+
+}
+
+function init()
+{
+    let game = new Game();
 }
